d9e8bc8d2d114d3473f08c9e5d57ed5fa2eeeed0
sadbatya <sadbatya@mail.ru>
2024-07-05 21:57:21 -0400

    add slider

---
+++ js/slider_move.js
@@ -0,0 +1,13 @@
+const sliderBtnPrev = document.querySelector(".btn-left");
+const sliderBtnNext = document.querySelector(".btn-right");
+
+const container = document.querySelector(".slider");
+const sliderContainer = document.querySelector(".slider__cards");
+const cards = sliderContainer.querySelector('.slider__card');
+
+export const  sliderMove = () => {
+  console.log(container);
+  console.log(sliderContainer);
+  console.log(cards);
+};
+
---
+++ script.js
@@ -0,0 +1,13 @@
+import { renderPlayers } from "./js/render_players.js";
+import { sliderMove } from "./js/slider_move.js";
+
+
+
+
+
+
+renderPlayers();
+sliderMove();
+
+
+
