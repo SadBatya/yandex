e203db537aa80f360ffc1ceea1173c9c0e053626
sadbatya <sadbatya@mail.ru>
2024-07-07 11:18:18 -0400

    add slider

--- js/render_players.js
+++ js/render_players.js
@@ -1,18 +1,19 @@
-import { sliderPlayers } from "../data/slider_players.js";
+// import { sliderPlayers } from "../data/slider_players.js";
+// import { sliderMove } from "./slider_move.js";
+// const sliderCards = document.querySelector(".slider__cards");
 
-const sliderCards = document.querySelector(".slider__cards");
-
-export function renderPlayers() {
-  sliderPlayers.forEach((player) => {
-    const cardHTML = `
-    <div class="slider__card">
-      <img class="slider__card-img" src="./assets/player.png" alt="" />
-      <div class="slider__card-name">${player.name}</div>
-      <div class="slider__card-status">${player.status}</div>
-      <button class="slider__card-button">Подробнее</button>
-    </div>
-    `;
-    sliderCards.innerHTML += cardHTML;
-  });
-}
+// export function renderPlayers() {
+//   sliderPlayers.forEach((player) => {
+//     const cardHTML = `
+//     <div class="slider__card">
+//       <img class="slider__card-img" src="./assets/player.png" alt="" />
+//       <div class="slider__card-name">${player.name}</div>
+//       <div class="slider__card-status">${player.status}</div>
+//       <button class="slider__card-button">Подробнее</button>
+//     </div>
+//     `;
+//     sliderCards.innerHTML += cardHTML;
+//   });
+//   sliderMove()
+// }
 
--- script.js
+++ script.js
@@ -1,13 +1,38 @@
-import { renderPlayers } from "./js/render_players.js";
-import { sliderMove } from "./js/slider_move.js";
+const btnPrev = document.querySelector(".btn-left");
+const btnNext = document.querySelector(".btn-right");
+const curentNumberCard = document.querySelector(
+  ".slider__title-number-current"
+);
+const sliderContainer = document.querySelector(".slider__cards");
+const sliderLine = document.querySelector(".slider__line");
+const card = document.querySelector(".slider__card");
+const cardWidth = card.offsetWidth;
+let currentIndex = 3;
+let position = 0;
 
+console.log(cardWidth);
+console.log(card);
 
+function updateBtnStatus() {
+  btnPrev.disabled = currentIndex == 3;
+  btnNext.disabled = currentIndex == 6;
+}
 
+btnNext.addEventListener("click", () => {
+  currentIndex++;
+  curentNumberCard.textContent = currentIndex;
+  position += cardWidth + 35;
+  sliderLine.style.transform = `translateX(-${position}px)`;
+  updateBtnStatus();
+});
 
+btnPrev.addEventListener("click", () => {
+  currentIndex--;
+  curentNumberCard.textContent = currentIndex;
+  position -= cardWidth + 35;
+  sliderLine.style.transform = `translateX(-${position}px)`;
+  updateBtnStatus();
+  console.log(position)
+});
 
-
-renderPlayers();
-sliderMove();
-
-
-
+updateBtnStatus();
